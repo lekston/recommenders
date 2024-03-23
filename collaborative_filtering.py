eb2af141b18be1b8709e22dacac430c0401b2b2d
Przemek Lekston <p.lekston@gmail.com>
2024-03-24 00:39:47 +0100

    Add merging of UserUserWeights from different workers

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -2,16 +2,19 @@ from __future__ import annotations
 
 import math
 import numpy as np
+import os
 import pandas as pd
 import sys
 import time
 
+from multiprocessing import Pool
 from sortedcollections import SortedList
 
 from scipy import sparse
 from scipy.stats import pearsonr
 from scipy.stats._result_classes import PearsonRResult
-from typing import List, NamedTuple, Set, Tuple
+from typing import List, NamedTuple, Set, Tuple, TypeAlias
+
 
 # NOTE: all sparse matrices / arrays use indexing that starts with 1 (userId & movieId)
 # work-around:
@@ -56,7 +59,6 @@ def build_rating_matrix() -> sparse.coo_matrix:
     return rating_matrix
 
 
-
 # TODO: train, dev, test split
 
 # Recommend:
@@ -65,6 +67,7 @@ def build_rating_matrix() -> sparse.coo_matrix:
 #   - for each user precompute an ordered dict storing correlations to the top-k other most
 #       similar/dissimilar users (in absolute terms)
 #   - store this as csv
+#   DONE!
 # 3. Compute predicted score
 # 4. Test on 'held-out' validation
 #       > select some random users and remove random ratings from them
@@ -90,11 +93,11 @@ Performance:
         Single core: 8.6*1e5 per 1min -> 51.6*1e6 per 1h -> pearsonr + searchsorted eat up 60% of CPU
     - min overlap 25, min p_value 0.35 (dummy Pearson, using searchsorted on sets)
         Single core: 6.6*1e5 per 1min -> 51.6*1e6 per 1h -> pearsonr alone uses ~25% of CPU
-    TODO: 
-    Option for speeding up: multiprocessing parallel for
-    - parallelize over blocks of traingular matrix
-    - gather all correlation values
-    - insert all results into the top_k_u2u shortlist (single threaded)
+
+    Multiprocessing parallelization"
+    DONE - parallelization is done over sub-triagnles of the traingular matrix
+    DONE - gather all correlation values
+    DONE - insert all results into the top_k_u2u shortlist (single threaded)
     TODO:
     - consider cosine instead of pearsonr as data is already centered (but I would still need the p-value)
 '''
@@ -109,31 +112,49 @@ class CorrelationEntry(NamedTuple):
     user_id: int
 
 
-class UserUserWeights(object):
-    SingleUserTopKSortedEntries = SortedList[CorrelationEntry]
+LOGGING_PERIOD = 20000
 
-    def __init__(self, num_of_users: int, top_k: int = 20, absolute_sort: bool = True):
+
+class UserUserWeights(object):
+    SingleUserTopKSortedEntries : TypeAlias = SortedList[CorrelationEntry]
+
+    def __init__(
+            self,
+            num_of_users: int,
+            top_k: int = 20,
+            sampling_ratio: float = 0.04,
+            absolute_sort: bool = True,
+        ):
         '''
         if absolute_sort == True: store both the most similar and the most dissimilar users
         '''
         super().__init__()
         assert num_of_users > 0  and top_k > 0
-        self._num_of_users = 0
+        self._num_of_users = num_of_users
         self._sorting_key = (lambda x: -math.fabs(x[0])) if absolute_sort else (lambda x: -x[0])
         self._overlap_abs_min = 25
         self._overlap_ratio = 0.5
         self._max_p_value = 0.05
-        self._sampling_ratio = 0.04
+        self._sampling_ratio = sampling_ratio
         self._top_k = top_k
-        self._top_k_u2u_correlations: List[self.SingleUserTopKSortedEntries] = [
+        self._top_k_u2u_correlations: List[UserUserWeights.SingleUserTopKSortedEntries] = [
             SortedList(key=self._sorting_key) for _ in range(0, num_of_users)
         ]
 
-    def __getitem__(self, index) -> List[SingleUserTopKSortedEntries]:
-        return list(self._top_k_u2u_correlations[index])
+    def __setitem__(self, key: int, value: UserUserWeights.SingleUserTopKSortedEntries):
+        self._top_k_u2u_correlations[key] = value
 
-    def get_correlations(self) -> List[CorrelationEntry]:
+    def __getitem__(self, key: int) -> UserUserWeights.SingleUserTopKSortedEntries:
+        return self._top_k_u2u_correlations[key]
+
+    def __len__(self) -> int:
+        return len(self._top_k_u2u_correlations)
+
+    def get_correlations_as_list(self) -> List[List[CorrelationEntry]]:
         return [[u2u_entry for u2u_entry in user] for user in self._top_k_u2u_correlations]
+
+    def get_correlations(self) -> List[UserUserWeights.SingleUserTopKSortedEntries]:
+        return self._top_k_u2u_correlations
 
     def load_from_matrix(self, centered_rating_matrix: sparse.csr_matrix, shard_id=0, shards_count=1):
         pid = os.getpid()
@@ -143,7 +164,7 @@ class UserUserWeights(object):
         #     return DummyPearsonr(statistic=random.uniform(-1.0, 1.0), pvalue=random.uniform(0.3,0.8))
         # pearsonr_algo = dummy_pearsonr_algo
         pearsonr_algo = pearsonr
-        def _calculate_corr_ref(user_i:sparse.csr_matrix, user_j:sparse.csr_matrix , common_item_indices: Set):
+        def _calculate_corr_ref(user_i:sparse.csr_matrix, user_j:sparse.csr_matrix , common_item_indices: Set[int]):
             '''
             Extract shared ratings i.e. for items that both users have rated and compute user-user correlation
             '''
@@ -153,13 +174,13 @@ class UserUserWeights(object):
                 user_i_rating_shortlist[out_idx] = user_i.data[user_i.indices.searchsorted(index_value)]
                 user_j_rating_shortlist[out_idx] = user_j.data[user_j.indices.searchsorted(index_value)]
             return pearsonr_algo(user_i_rating_shortlist, user_j_rating_shortlist)
-        def _calculate_corr_array_like(user_i:sparse.csr_matrix, user_j:sparse.csr_matrix , common_item_indices: Set):
+        def _calculate_corr_array_like(user_i:sparse.csr_matrix, user_j:sparse.csr_matrix , common_item_indices: Set[int]):
             '''
             Extract shared ratings i.e. for items that both users have rated and compute user-user correlation
             '''
-            common_item_indices = sorted(common_item_indices)
-            user_i_csr_indices = user_i.indices.searchsorted(common_item_indices)
-            user_j_csr_indices = user_j.indices.searchsorted(common_item_indices)
+            sorted_common_item_indices: List[int] = sorted(common_item_indices)
+            user_i_csr_indices = user_i.indices.searchsorted(sorted_common_item_indices)
+            user_j_csr_indices = user_j.indices.searchsorted(sorted_common_item_indices)
             return pearsonr_algo(user_i.data[user_i_csr_indices], user_j.data[user_j_csr_indices])
 
         start_time = time.time()
@@ -182,7 +203,7 @@ class UserUserWeights(object):
                     # corr_value: PearsonRResult = _calculate_corr_ref(user_i, user_j, common_item_indices)
                     corr_value: PearsonRResult = _calculate_corr_array_like(user_i, user_j, common_item_indices)
                     if corr_value.pvalue > self._max_p_value:
-                        self.add_entry((user_i_idx, user_j_idx), corr_value.statistic)
+                        self.add_paired_entry((user_i_idx, user_j_idx), corr_value.statistic)
                         num_updated += 1
                 else:
                     if len(common_item_indices) > self._overlap_abs_min:
@@ -195,53 +216,91 @@ class UserUserWeights(object):
                     print(f"[{shard_id+1}/{shards_count}] Elapsed time: {time.time() - start_time:.4f}\t"
                           f"Explored: {num_of_explored_relations}\tUpdated: {num_updated} (i,j): {user_i_idx, user_j_idx}")
 
-    def add_entry(self, two_users: Tuple[int, int], corr_value: int):
+    def add_paired_entry(self, two_users: Tuple[int, int], corr_value: int):
         user_i, user_j = two_users
-        def _update_user(user, other_user, value):
-            user_top_k_entries: SingleUserTopKSortedEntries = self._top_k_u2u_correlations[user]
+        def _update_user(user: int, other_user: int, value: int) -> UserUserWeights.SingleUserTopKSortedEntries:
+            user_top_k_entries: UserUserWeights.SingleUserTopKSortedEntries = self._top_k_u2u_correlations[user]
             user_top_k_entries.add((value, other_user))
             if len(user_top_k_entries) > self._top_k:
                 user_top_k_entries.pop()
-        
+
         _update_user(user_i, user_j, corr_value)
         _update_user(user_j, user_i, corr_value)
+
+    def recreate_entry(self, idx: int, user: SingleUserTopKSortedEntries):
+        self._top_k_u2u_correlations[idx] = user
 
     @property
     def top_k_u2u_correlations(self):
         return self._top_k_u2u_correlations # TODO: convert to dataframe??
 
-    @staticmethod
-    def serialize(top_k_u2u_correlations: List[SingleUserTopKSortedEntries], filename: str):
-        # import pdb; pdb.set_trace()
+    @classmethod
+    def serialize(cls, top_k_u2u_correlations: List[SingleUserTopKSortedEntries], filename: str):
         total_written = 0
         u2u_corr_list = [(idx, l) for idx, l in enumerate(top_k_u2u_correlations) if len(l) > 0]
         with open(filename, 'w') as ofile:
             for idx, row in u2u_corr_list:
-                output = [str(entry) for entry in row]
-                ofile.write(f'{ idx},\t' + ', '.join(output) + '\n')
+                output = [str(entry) for entry_tuple in row for entry in entry_tuple]
+                ofile.write(f'{ idx}, ' + ', '.join(output) + '\n')
                 total_written += 1
-                if total_written % 10000 == 0:
+                if total_written % LOGGING_PERIOD == 0:
                     print(f"Printed {total_written} lines")
-        # import pdb; pdb.set_trace()
-        # pass
-    
+
     @staticmethod
-    def build_from_file(filename: str) -> UserUserWeights:
-        # TODO: TESTME
-        import pdb; pdb.set_trace()
-        data = pd.read_csv(filename) # TODO
-        u2u_weights = UserUserWeights(num_of_users=1)
-        u2u_weights._num_of_users = data.shape[0]
-        u2u_weights._top_k = data.shape[1] # or max len of the longest row
-        u2u_weights._top_k_u2u_correlations = data # TODO: is it worth converting back to list of sorted lists??
-        return u2u_weights
+    def build_from_files(filenames: List[str]) -> UserUserWeights:
+        forced_columns = [chr(ord('A')+i) for i in range(0,26)]
+        u2u_parts = []
+        for fname in filenames:
+            data = pd.read_csv(
+                fname, header=None,
+                names=forced_columns,
+                skipinitialspace=True,
+                escapechar=r"|", doublequote=False # data for legacy experiments
+            )
+            last_user_idx = int(data.iloc[-1, 0])
+            u2u_weights = UserUserWeights(
+                num_of_users=max(data.shape[0]+1, last_user_idx+1),
+                top_k=20
+            )
+            # data.shape[1] expected to hold the max len of the longest row
+
+            for index, row in data.iterrows():
+                user_id = row.iloc[0].astype(int)
+                user_entries = [
+                    CorrelationEntry(corr, other_uid)
+                    for corr, other_uid
+                    in zip(row.iloc[1::2], row.iloc[2::2])
+                    if not math.isnan(corr + other_uid)
+                ]
+                u2u_weights.recreate_entry(user_id, user=SortedList(
+                    iterable=user_entries,
+                    key=u2u_weights._sorting_key
+                ))
+                if int(index) % LOGGING_PERIOD == 0:
+                    print(f"file: {fname}:{index}")
+            u2u_parts.append(u2u_weights)
+
+        u2u_parts = sorted(u2u_parts, key=lambda x: len(x), reverse=True)
+        u2u_weights_merged = u2u_parts[0]
+        for part_idx, part in enumerate(u2u_parts[1:]):
+            u2u_weights_merged.merge(part)
+            print(f"Merged parts: base + {part_idx + 1}")
+        return u2u_weights_merged
+
+    def merge(self, other_u2u_weights: UserUserWeights):
+        for other_uid, single_user_row in enumerate(other_u2u_weights.get_correlations()):
+            if len(self[other_uid]) == 0:
+                self[other_uid] = single_user_row
+            else:
+                self[other_uid].update(single_user_row)
+                pass
+            row = self[other_uid]
+            while len(row) > self._top_k:
+                row.pop()
+        return self
 
 
-from multiprocessing import Pool
-import os
-
-
-if __name__ == '__main__':
+def generate_user_user_matrix(pool_size: int = 8) -> None:
     rating_matrix: sparse.csr_matrix = build_rating_matrix().tocsr() # Compressed sparse row for efficient compute
     rating_matrix.sort_indices() # in-place
 
@@ -283,10 +342,9 @@ if __name__ == '__main__':
         rmat_centered, shard_id, shards_count = rmat_centered_with_shard_details
         u2u_w = UserUserWeights(num_of_users=rmat_centered.shape[0], top_k=10)
         u2u_w.load_from_matrix(rmat_centered, shard_id, shards_count)
-        return u2u_w.get_correlations()
+        return u2u_w.get_correlations_as_list()
 
     start_time = time.time()
-    pool_size = 8
     if pool_size > 1:
         with Pool(pool_size) as p:
             u2u_weights_to_merge = p.map(
@@ -300,3 +358,16 @@ if __name__ == '__main__':
 
     print("Done")
     print(f"Time spend on user-user correlations: {time.time() - start_time}")
+
+
+# head -n 10 u2u_demo_0.csv | sed -E "s/\(([^)]*),([^)]*)\)/'\1|,\2'/g" > u2u_test_inputs_1.csv
+
+if __name__ == '__main__':
+    # UserUserWeights.build_from_files([f"u2u_demo_{idx}.csv" for idx in range(0, 1)])
+    u2u_merged = UserUserWeights.build_from_files([
+        f"u2u_test_inputs_{idx}.csv" for idx in range(0, 8)
+    ])
+    print(len(u2u_merged))
+    all_lengths = [(idx, len(entry)) for idx, entry in enumerate(u2u_merged.get_correlations())]
+    print(len(['' for idx, entry_len in all_lengths if entry_len > 0]))
+    import pdb; pdb.set_trace()
